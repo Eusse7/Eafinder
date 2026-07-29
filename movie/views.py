13b8eddad8912d60720c23bbfa5bb598ea39855e
juan.urrego <juan.urrego@quipux.com>
2026-07-29 11:48:44 -0500

    Feat: se agrega el módulo de peliculas y la visualizacion de estas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     # return HttpResponse('<h1>Bienvenido a la página de inicio de Movie Reviews</h1>')
-    return render(request, 'home.html', {'name':'Juan García'})
+    # return render(request, 'home.html', {'name':'Juan García'})
+    searchTerm = request.GET.get('searchMovie')
+    
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     # return HttpResponse('<h1>Acerca de Movie Reviews</h1><p>Esta es una aplicación para revisar películas.</p>')
