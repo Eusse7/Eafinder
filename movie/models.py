215ba1e2e1028f94796c479a19b70a7ac71c8057
Eusse7 <afeussec@eafit.edu.co>
2026-07-29 11:24:25 -0500

    Primer envio de cambios

---
+++ movie/models.py
@@ -0,0 +1,17 @@
+"""
+Modelos de la aplicación movie.
+"""
+from django.db import models
+
+# Create your models here.
+
+
+class Movie(models.Model):
+    """
+    Modelo para representar una película en el sistema.
+    """
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+    
