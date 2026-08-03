8911a178c695e3a189f50c470d2db1a9e7d21d23
Yesid16 <hyesid633@gmail.com>
2026-08-03 17:56:42 -0500

    Visualización de Películas con estilos

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Movie
 # Create your views here.
 
 def home(request):
    # return HttpResponse('<h1> Welcome to my page </h1>')
    # return render(request, 'home.html')
-   return render(request, 'home.html', {'name': 'Yesid Hurtado Montoya'})
+   #return render(request, 'home.html', {'name': 'Yesid Hurtado Montoya'})
+    searchTerm = request.GET.get('searchMovie')
 
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(
+        request,
+        'home.html',
+        {
+            'searchTerm': searchTerm,
+            'movies': movies
+        }
+    )
 def about(request):
-    return HttpResponse('<h1> Welcome to my about page </h1>')
+   return render(request, 'about.html', {'name': 'Yesid Hurtado Montoya'})
