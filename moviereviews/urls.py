0f657736cdc72c984fee8fa5289327d4e65dfd36
juan.urrego <juan.urrego@quipux.com>
2026-08-30 14:38:50 -0500

    creacion de app news

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -19,12 +19,14 @@ from django.urls import path
 from movie import views as movieViews
 
 from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home, name='home'),
     path('about/', movieViews.about, name='about'),
+    path('news/', include('news.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
