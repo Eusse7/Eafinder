c6fd3fb57e9087c0526c542ef23452b3c2b373b8
juan.urrego <juan.urrego@quipux.com>
2026-08-31 12:19:48 -0500

    Llenado de la base de datos de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,4 @@
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 # Create your models here.
@@ -5,5 +6,14 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='movie/images/')
+    image = models.ImageField(
+        upload_to='movie/images/',
+        storage=FileSystemStorage(allow_overwrite=True),
+        default='movie/images/default.jpg',
+    )
     url = models.URLField(blank=True)
+    genre = models.CharField(max_length=100, blank=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
