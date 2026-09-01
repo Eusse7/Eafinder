0b4aa00fd2a5cb61c9e8c40adb8b350554cc4279
juanji29 <jejimenez1@eafit.edu.co>
2026-09-01 11:24:59 -0500

    Generacion de graficas

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from movie import views as movieViews
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -24,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movie.urls')),
     path('news/', include('news.urls')),
+    path('statistics/', movieViews.statistics_view, name ='statistics'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
